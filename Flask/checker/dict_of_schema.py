eefbb14d6133395bc0be4a8ca17b072762133032
reddid <forgothowtoreddid@gmail.com>
2023-12-07 16:59:39 +0100

    first commit

--- Flask/checker/dict_of_schema.py
+++ Flask/checker/dict_of_schema.py
@@ -1,3 +1,18 @@
+'''Copyright (C) 2023 DISIT Lab http://www.disit.org - University of Florence
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.'''
+
 import json
 
 def parse_mysql_dump(dump_file_path):
@@ -8,15 +23,12 @@ def parse_mysql_dump(dump_file_path):
         lines = file.readlines()
 
         for line in lines:
-            #line = line.strip()
-
-            # Identify table creation statements
+            
             if line.startswith('CREATE TABLE'):
                 table_name = line.split('`')[1]
                 current_table = table_name
                 schema[current_table] = []
             
-            # Extract column definitions
             elif line.startswith('  `'):
                 if current_table:
                     parts = line.split('`')
@@ -25,8 +37,7 @@ def parse_mysql_dump(dump_file_path):
                     schema[current_table].append({'Field': column_name, 'Type': column_type})
 
     return schema
-
-# Replace 'dump_file.sql' with your MySQL dump file path
+# replace 'dump_file.sql' with your MySQL dump file path
 dump_file_path = 'out.sql'
 
 result = parse_mysql_dump(dump_file_path)
