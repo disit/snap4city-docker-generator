85d403c0b127fd64b8b9cfc7074b256d6bec7e82
reddid <forgothowtoreddid@gmail.com>
2024-03-28 11:06:50 +0100

    minor fixes and updates

--- Flask/Modules/database/filemanager.sql
+++ Flask/Modules/database/filemanager.sql
@@ -3,28 +3,28 @@ CREATE DATABASE filemanagerdb;
 USE filemanagerdb;
 
 CREATE TABLE fileextensions (
-id INT NOT NULL AUTO_INCREMENT,
+`id` INT NOT NULL AUTO_INCREMENT,
 extension VARCHAR(30) NOT NULL,
-PRIMARY KEY (‘id’))
+PRIMARY KEY (`id`));
 
-INSERT INTO fileextensions (extension) VALUE (“img”);
-INSERT INTO fileextensions (extension) VALUE (“png”);
-INSERT INTO fileextensions (extension) VALUE (“jpg”);
-INSERT INTO fileextensions (extension) VALUE (“pdf”);
-INSERT INTO fileextensions (extension) VALUE (“ps”);
-INSERT INTO fileextensions (extension) VALUE (“zip”);
-INSERT INTO fileextensions (extension) VALUE (“rar”);
-INSERT INTO fileextensions (extension) VALUE (“tgz”);
-INSERT INTO fileextensions (extension) VALUE (“doc”);
-INSERT INTO fileextensions (extension) VALUE (“docx”);
-INSERT INTO fileextensions (extension) VALUE (“xls”);
-INSERT INTO fileextensions (extension) VALUE (“xlsx”);
-INSERT INTO fileextensions (extension) VALUE (“ppt”);
-INSERT INTO fileextensions (extension) VALUE (“pptx”);
-INSERT INTO fileextensions (extension) VALUE (“tar”);
-INSERT INTO fileextensions (extension) VALUE (“pcx”);
-INSERT INTO fileextensions (extension) VALUE (“html”);
-INSERT INTO fileextensions (extension) VALUE (“css”);
-INSERT INTO fileextensions (extension) VALUE (“dat”);
-INSERT INTO fileextensions (extension) VALUE (“py”);
-INSERT INTO fileextensions (extension) VALUE (“r”);
+INSERT INTO fileextensions (extension) VALUES ("img");
+INSERT INTO fileextensions (extension) VALUES ("png");
+INSERT INTO fileextensions (extension) VALUES ("jpg");
+INSERT INTO fileextensions (extension) VALUES ("pdf");
+INSERT INTO fileextensions (extension) VALUES ("ps");
+INSERT INTO fileextensions (extension) VALUES ("zip");
+INSERT INTO fileextensions (extension) VALUES ("rar");
+INSERT INTO fileextensions (extension) VALUES ("tgz");
+INSERT INTO fileextensions (extension) VALUES ("doc");
+INSERT INTO fileextensions (extension) VALUES ("docx");
+INSERT INTO fileextensions (extension) VALUES ("xls");
+INSERT INTO fileextensions (extension) VALUES ("xlsx");
+INSERT INTO fileextensions (extension) VALUES ("ppt");
+INSERT INTO fileextensions (extension) VALUES ("pptx");
+INSERT INTO fileextensions (extension) VALUES ("tar");
+INSERT INTO fileextensions (extension) VALUES ("pcx");
+INSERT INTO fileextensions (extension) VALUES ("html");
+INSERT INTO fileextensions (extension) VALUES ("css");
+INSERT INTO fileextensions (extension) VALUES ("dat");
+INSERT INTO fileextensions (extension) VALUES ("py");
+INSERT INTO fileextensions (extension) VALUES ("r");
