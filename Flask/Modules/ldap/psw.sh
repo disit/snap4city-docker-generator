e89b1711695976478952b670be3a1e5829a5b645
reddid <forgothowtoreddid@gmail.com>
2024-08-05 23:23:09 +0200

    fixes to psw generation

--- Flask/Modules/ldap/psw.sh
+++ Flask/Modules/ldap/psw.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #generates an hash for the password $#ldap-admin-pwd#$
 psw=$(/usr/sbin/slappasswd -h {SSHA} -s "$#ldap-admin-pwd#$")
-pswadmin=$(/usr/sbin/slappasswd -h {SSHA} -s "$#dashboard-personaldata-user-pwd#$")
+pswadmin=$(/usr/sbin/slappasswd -h {SSHA} -s "$#userrootadmin-pwd#$")
 pswaream=$(/usr/sbin/slappasswd -h {SSHA} -s "$#areamanager-pwd#$")
 pswuserm=$(/usr/sbin/slappasswd -h {SSHA} -s "$#usermanager-pwd#$")
 pswusert=$(/usr/sbin/slappasswd -h {SSHA} -s "$#tool-admin-pwd#$")
@@ -15,4 +15,5 @@ sed -i "s@oldUserManagerPW@$pswuserm@" "/ldif_files/admin_update_fixed.ldif"
 sed -i "s@oldToolAdminPW@$pswusert@" "/ldif_files/admin_update_fixed.ldif"
 ldapmodify -H ldapi:// -Y EXTERNAL -f /ldif_files/psw_update_fixed.ldif
 rm /ldif_files/psw_update_fixed.ldif
-#ldappasswd -H ldap://localhost -x -D "cn=admin,dc=ldap,dc=organization,dc=com" -w secret -a secret -s $psw
+ldapmodify -H ldap://localhost -x -D "cn=admin,dc=ldap,dc=organization,dc=com" -w $#ldap-admin-pwd#$ -f /ldif_files/admin_update_fixed.ldif
+rm /ldif_files/admin_update_fixed.ldif
