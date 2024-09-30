cacd172d9dff175269b4f9cdf37a81123f863161
ElMustacho <fabri.mereu@gmail.com>
2024-09-30 11:58:41 +0200

    fix

--- Flask/Modules/post-setup-kubernetes.sh
+++ Flask/Modules/post-setup-kubernetes.sh
@@ -1,4 +1,11 @@
 # note: this is still experimental
+kubectl -n $#k8-namespace#$ exec deployment/servicemap  --  bash -c "cd /root/servicemap; ./update-ontology-k8.sh virtuoso-kb"
+kubectl -n $#k8-namespace#$ exec deployment/virtuoso-kb  --  isql-v localhost dba "$#virtuoso-kb-pwd#$" "EXEC=rdfs_rule_set ('urn:ontology', 'http://www.disit.org/km4city/resource/Ontology');"
+kubectl -n $#k8-namespace#$ exec deployment/virtuoso-kb  --  isql-v localhost dba "$#virtuoso-kb-pwd#$" /root/servicemap/servicemap.vt
+kubectl -n $#k8-namespace#$ exec deployment/virtuoso-kb  --  isql-v localhost dba "$#virtuoso-kb-pwd#$" /root/servicemap/valuetypes.vt
+kubectl -n $#k8-namespace#$ exec deployment/virtuoso-kb  --  isql-v localhost dba "$#virtuoso-kb-pwd#$" /root/servicemap/servicemap-dbpedia.vt
+
+
 cd servicemap-conf
 
 
