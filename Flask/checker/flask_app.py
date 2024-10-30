242b52f022a928a259d14c79ed76b81cad600c87
ElMustacho <fabri.mereu@gmail.com>
2024-10-30 16:33:20 +0100

    tentative top inclusion on aitoalert

--- Flask/checker/flask_app.py
+++ Flask/checker/flask_app.py
@@ -234,15 +234,23 @@ def auto_alert_status():
     names_of_problematic_containers = [n["Names"] for n in problematic_containers]
     containers_which_are_not_expected = list(set(components_original)-set([a["Names"] for a in containers_merged]))
     containers_which_are_not_expected = [a for a in containers_which_are_not_expected if not a.endswith("*")]
+    top = get_top()
+    load_averages = re.findall(r"(\d+\.\d+)", top["system_info"]["load_average"])[-3:]
+    load_issues=""
+    for average, timing in zip(load_averages, [1, 5, 15]):
+        if average > config["load-threshold"]:
+            load_issues += "Load threshold above "+str(config["load-threshold"]) + " with " + str(average) + "during the last " + str(timing) + " minute(s).\n"
     if len(names_of_problematic_containers) > 0 or len(is_alive_with_ports) > 0 or len(containers_which_are_not_expected):
         try:
-            issues = ["","",""]
+            issues = ["","","",""]
             if len(names_of_problematic_containers) > 0:
                 issues[0]=problematic_containers
             if len(is_alive_with_ports) > 0:
                 issues[1]=is_alive_with_ports
             if len(containers_which_are_not_expected) > 0:
                 issues[2]=containers_which_are_not_expected
+            if len(load_issues)>0:
+                issues[3]=load_issues
             send_advanced_alerts(issues)
         except Exception:
             print(traceback.format_exc())
@@ -330,7 +338,6 @@ def get_top():
                 'command': ' '.join(columns[11:])
             }
             parsed_data['processes'].append(process_info)
-    
     return parsed_data
 
 def send_alerts(message):
@@ -353,9 +360,11 @@ def send_advanced_alerts(message):
         if len(message[2])>0:
             em3 = format_error_to_send("wasn't found running in docker ",", ".join(message[2]))
             text_for_email+= "These containers weren't found in docker: "+ ", ".join(message[2])+"\n"
+        if len(message[3])>0:
+            text_for_email+= message[3]
         try:
-            if len(em1+"\n"+em2+"\n"+em3) > 5:
-                send_email(config["sender-email"], config["sender-email-password"], config["email-recipients"], config["platform-url"]+" is in trouble!", em1+"\n"+em2+"\n"+em3)
+            if len(text_for_email) > 5:
+                send_email(config["sender-email"], config["sender-email-password"], config["email-recipients"], config["platform-url"]+" is in trouble!", em1+"\n"+em2+"\n"+em3+"\n"+message[3])
         except:
             print("[ERROR] while sending email:",text_for_email)
         text_for_telegram, t1, t2, t3 = "", "", "", ""
@@ -368,11 +377,13 @@ def send_advanced_alerts(message):
         if len(filter_out_muted_containers_for_telegram(message[2]))>0:
             t3=format_error_to_send("wasn't found running in docker ",filter_out_muted_containers_for_telegram(message[2]))
             text_for_telegram+= "These containers weren't found in docker: "+ str(filter_out_muted_containers_for_telegram(message[2]))+"\n"
+        if len(message[3])>0:
+            text_for_telegram+= message[3]
         if len(text_for_telegram)>5:  #todo check me better
             try:
-                send_telegram(config['telegram-channel'], t1+"\n"+t2+"\n"+t3)
+                send_telegram(config['telegram-channel'], t1+"\n"+t2+"\n"+t3+"\n"+message[3])
             except:
-                print("[ERROR] while sending telegram:",t1+"\n"+t2+"\n"+t3,"\nDue to",traceback.format_exc())
+                print("[ERROR] while sending telegram:",t1+"\n"+t2+"\n"+t3+"\n"+message[3],"\nDue to",traceback.format_exc())
     except Exception:
         print("Error sending alerts:",traceback.format_exc())
         
