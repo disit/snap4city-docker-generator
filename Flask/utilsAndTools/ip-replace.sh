549ed52682972d5d3525375353fe15b61cf806b2
ElMustacho <fabri.mereu@gmail.com>
2024-10-30 09:49:02 +0100

    broader solution to ip replacement in kubernetes models

--- Flask/utilsAndTools/ip-replace.sh
+++ Flask/utilsAndTools/ip-replace.sh
@@ -5,6 +5,11 @@ echo "Enter the replacement string for 127.0.0.1:"
 read replacement_string
 for yaml_file in "$script_dir"/*.yaml; do
 
+    if [ "$(basename "$yaml_file")" = "kafka-deployment-new.yaml" ]; then
+            echo "Skipping file: $yaml_file"
+            continue
+        fi
+
     if [ "$(basename "$yaml_file")" = "kafka-deployment.yaml" ]; then
             echo "Skipping file: $yaml_file"
             continue
