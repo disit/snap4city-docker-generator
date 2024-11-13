6d75a89625bfa0d3812e19c82d9feb6e411cf686
ElMustacho <fabri.mereu@gmail.com>
2024-11-13 11:46:32 +0100

    Update flask_app_multi.py
    double definition fixed

--- Flask/checker/flask_app_multi.py
+++ Flask/checker/flask_app_multi.py
@@ -80,23 +80,6 @@ def send_telegram(chat_id, message):
     asyncio.run(bot.send_message(chat_id=chat_id, text=str(message)))
     return
 
-def format_error_to_send(instance_of_problem, containers, because = None, explain_reason=None):
-    using_these = ', '.join('"{0}"'.format(w) for w in containers.split(","))
-    if because:
-        becauses=because.split(",")
-    with mysql.connector.connect(**db_conn_info) as conn:
-        cursor = conn.cursor(buffered=True)
-        query2 = 'SELECT category, component, position FROM checker.component_to_category where component in ({}) order by category;'.format(using_these)
-        cursor.execute(query2)
-        now_it_is = cursor.fetchall()
-    newstr=""
-    for a in now_it_is:
-        curstr="In category " + a[0] + ", located in " + a[2] + " the docker container named " + a[1] + " " + instance_of_problem
-        if because:
-            newstr += curstr + explain_reason + becauses.pop(0)+"\n"
-        else:
-            newstr += curstr+"\n"
-    return newstr
 
 def get_top():
     process = subprocess.Popen(['top', '-b', '-n', '1'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
