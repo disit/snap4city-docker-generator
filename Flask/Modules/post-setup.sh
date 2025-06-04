ce9a6fccacf1cc7bca96f8845ade859aa9831729
ElMustacho <fabri.mereu@gmail.com>
2025-06-04 12:08:53 +0200

    commit for updates on 2025-05-04

--- Flask/Modules/post-setup.sh
+++ Flask/Modules/post-setup.sh
@@ -130,6 +130,7 @@ curl --insecure -u admin:$#opensearch-admin-pwd#$ -XPOST "http://localhost:5601/
 echo
 
 docker compose exec dashboard-builder bash -c "cd /var/www/html/dashboardSmartCity/sql; php updateDb.php"
+docker compose exec dashboard-builder bash -c "cd /var/www/html/iot-directory/sql; php updateDb.php"
 docker compose exec dashboard-cron bash -c "cd /var/www/html/dashboardSmartCity/opensearch; php IngestData.php"
 
 echo add geoserver workspace Snap4City - if this command fails, it might be because this machine is not able to resolve its own name\; consider using localhost in such a case
