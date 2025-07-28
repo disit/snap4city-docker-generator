5ed63a2e5fc03edc8d4ba05aff0f5738883cb5f1
ElMustacho <fabri.mereu@gmail.com>
2025-07-28 20:44:08 +0200

    fixes

--- Flask/Modules/dashboard-builder-conf/ssl_expose.php
+++ Flask/Modules/dashboard-builder-conf/ssl_expose.php
@@ -5,7 +5,7 @@ $key = hash('sha256', '$#aes-encryption-key-16chars#$');
 
 $iv = substr(hash('sha256', '$#aes-encryption-iv-16chars#$'), 0, 16);
 
-$output = openssl_encrypt('$#virtuoso-kb-pwd#$', 'AES-256-CBC', $key, 0, $iv);
+$output = openssl_encrypt('Anonymous', 'AES-256-CBC', $key, 0, $iv);
 
 $output = base64_encode($output);
 
